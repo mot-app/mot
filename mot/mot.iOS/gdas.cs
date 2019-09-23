d592e0f38304b749d4d30f134e60e8776723b70f
Morten Helland <mortenhelland@outlook.com>
2019-09-23 19:53:43 +1000

    :construction:

---
+++ mot/mot.iOS/gdas.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace mot.iOS
+{
+    public partial class gdas : UIImageView
+    {
+        public gdas (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
