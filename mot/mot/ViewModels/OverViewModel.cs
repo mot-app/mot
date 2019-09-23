4540faced1c798f396d57a6e24834e0ebb4db39f
Andreas Garvik <andreas.garvik13@gmail.com>
2019-09-23 18:57:32 +1000

    :sparkles: Overview calls for users but need more work

--- mot/mot/ViewModels/OverViewModel.cs
+++ mot/mot/ViewModels/OverViewModel.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 
 namespace mot.ViewModels
 {
@@ -14,9 +15,11 @@ namespace mot.ViewModels
         public OverViewModel()
         {
             Task.Run(async () => await GetMeetups());
+            Task.Run(async () => await GetAvailableUsers());
         }
 
         public ObservableCollection<Meetup> Meetups { get; set; }
+        public ObservableCollection<User> Users { get; set; }
 
         private async Task GetMeetups()
         {
@@ -24,5 +27,16 @@ namespace mot.ViewModels
             string data = await RestService.Read(Uri);
             Meetups = new ObservableCollection<Meetup>(JsonConvert.DeserializeObject<List<Meetup>>(data));
         }
+
+        private async Task GetAvailableUsers()
+        {
+            var Uri = new Uri("https://server-cy3lzdr3na-uc.a.run.app/user");
+            string data = await RestService.Read(Uri);
+            var users = JsonConvert.DeserializeObject<List<User>>(data);
+            string id = await SecureStorage.GetAsync("id");
+            users.RemoveAll(user => !user.Available || !user.Busy || user.Id == id);
+            Users = new ObservableCollection<User>(users);
+        }
+
     }
 }
