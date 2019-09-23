9a61ac56990a358c359f98ee69d86be2c2bdfc31
Morten Helland <mortenhelland@outlook.com>
2019-09-23 16:35:14 +1000

    :construction:
    Splash added on android

--- mot/mot.Android/MainActivity.cs
+++ mot/mot.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Android.OS;
 
 namespace mot.Droid
 {
-    [Activity(Label = "mot", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "mot", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
